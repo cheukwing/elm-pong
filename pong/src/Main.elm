a33970e6d88a81b6c9e89d3450e6b9fd0bd30d2f
Michael Tsang <michaeltsang1998@gmail.com>
2019-03-31 15:54:34 +0100

    Add single paddle with moving

--- pong/src/Main.elm
+++ pong/src/Main.elm
@@ -1,20 +1,45 @@
-module Main exposing (..)
+module Main exposing (Model, Msg(..), init, main, update, view)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
+import Browser.Events as Events
+import Html exposing (Html, div, h1, img, text)
 import Html.Attributes exposing (src)
+import Json.Decode as D
+import Svg as S
+import Svg.Attributes as SA
+
+
+
+--- CONSTANTS ---
+
+
+gameWidth =
+    500
+
+
+gameHeight =
+    500
+
+
+paddleWidth =
+    100
+
+
+paddleHeight =
+    10
+
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    {}
+    { position : Int }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { position = 0 }, Cmd.none )
 
 
 
@@ -22,12 +47,53 @@ init =
 
 
 type Msg
-    = NoOp
+    = Move Direction
+
+
+type Direction
+    = Left
+    | Right
+    | None
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        Move Left ->
+            ( { model | position = max 0 (model.position - 10) }, Cmd.none )
+
+        Move Right ->
+            ( { model | position = min (gameWidth - paddleWidth) (model.position + 10) }, Cmd.none )
+
+        _ ->
+            ( model, Cmd.none )
+
+
+
+--- SUBSCRIPTIONS ---
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Events.onKeyDown (D.map Move keyDecoder)
+
+
+keyDecoder : D.Decoder Direction
+keyDecoder =
+    D.map toDirection (D.field "key" D.string)
+
+
+toDirection : String -> Direction
+toDirection s =
+    case s of
+        "ArrowLeft" ->
+            Left
+
+        "ArrowRight" ->
+            Right
+
+        _ ->
+            None
 
 
 
@@ -37,8 +103,20 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        [ h1 [] [ text "Elm Pong" ]
+        , S.svg
+            [ SA.width (String.fromInt gameWidth)
+            , SA.height (String.fromInt gameHeight)
+            , SA.viewBox ("0 0 " ++ String.fromInt gameWidth ++ " " ++ String.fromInt gameHeight)
+            ]
+            [ S.rect
+                [ SA.x (String.fromInt model.position)
+                , SA.y (String.fromInt (gameHeight - 2 * paddleHeight))
+                , SA.width (String.fromInt paddleWidth)
+                , SA.height (String.fromInt paddleHeight)
+                ]
+                []
+            ]
         ]
 
 
@@ -52,5 +130,5 @@ main =
         { view = view
         , init = \_ -> init
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         }
